a35d0c50d2ae1482534cef7b9ea1ae962d4d34c7
Vincent Kang <vinkang97@gmail.com>
2016-12-08 18:51:44 -0500

    Finish HowTo Page

--- app/src/main/java/com/vince/socius/User.java
+++ app/src/main/java/com/vince/socius/User.java
@@ -2,9 +2,11 @@ package com.vince.socius;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,6 +20,9 @@ public class User extends AppCompatActivity {
     private String uid;
     private boolean isStaff;
     Button submit;
+    Button howButton;
+    Button whyButton;
+    TextView mainMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +31,9 @@ public class User extends AppCompatActivity {
         isStaff = false;
 
         submit = (Button)findViewById(R.id.moveButton);
+        whyButton = (Button) findViewById(R.id.whoWeAre);
+        howButton = (Button) findViewById(R.id.howToUse);
+        mainMessage = (TextView) findViewById(R.id.textView4);
 
         Intent intent = getIntent();
         uid = intent.getStringExtra(MainActivity.EXTRA_UID);
@@ -77,6 +85,28 @@ public class User extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void whoClicked(View view){
+        whyButton.setBackgroundResource(R.drawable.testback);
+        howButton.setBackgroundResource(R.drawable.testback2);
+        mainMessage.setText("Socius is an app that uses your contributions to track the needs of the " +
+                "homeless comunity across the city. We're in Alpha now, but we're hoping to grow our service " +
+                "so that the location data we collect can be used to " +
+                "deliver services directly to those who need it most.");
+        whyButton.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        howButton.setTextColor(ContextCompat.getColor(this,R.color.colorAccent));
+    }
+
+    public void howClicked (View view){
+        howButton.setBackgroundResource(R.drawable.testback);
+        whyButton.setBackgroundResource(R.drawable.testback2);
+        mainMessage.setText("When you encounter someone who you think could use homelessness" +
+                " services, let us know through the app. Eventually, we will have connections with" +
+                " community partners who can address immediate needs. Please be aware that, " +
+                "currently, we can only record need");
+        howButton.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        whyButton.setTextColor(ContextCompat.getColor(this,R.color.colorAccent));
     }
 
 }
