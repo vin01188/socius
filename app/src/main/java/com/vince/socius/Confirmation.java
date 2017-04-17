f2a48978341da06aa65e406956c5642772d86a88
Vincent Kang <vinkang97@gmail.com>
2017-04-17 17:52:51 -0400

    Finished expand click submenu

--- app/src/main/java/com/vince/socius/Confirmation.java
+++ app/src/main/java/com/vince/socius/Confirmation.java
@@ -43,6 +43,57 @@ public class Confirmation extends AppCompatActivity implements AdapterView.OnIte
     boolean medical;
     boolean toilet;
 
+
+    //food subCategories
+    CheckBox coffee;
+    CheckBox cannedGoods;
+    CheckBox sandwiches;
+    CheckBox fruit;
+    CheckBox water;
+    CheckBox granolaBars;
+
+    //clothes subCategories
+    CheckBox jeans;
+    CheckBox socks;
+    CheckBox coats;
+    CheckBox hats;
+    CheckBox gloves;
+    CheckBox blankets;
+    CheckBox shoes;
+    CheckBox boots;
+    CheckBox underwear;
+
+    //Toiletries Subcategories
+    CheckBox wetWipes;
+    CheckBox feminineCareProducts;
+    CheckBox handSanitizer;
+    CheckBox soap;
+    CheckBox deodorant;
+    CheckBox shampoo;
+    CheckBox lotion;
+    CheckBox toiletPaper;
+
+    //Camp supplies
+    CheckBox trashBags;
+    CheckBox tents;
+    CheckBox sleepingBags;
+    CheckBox rope;
+    CheckBox tarps;
+    CheckBox tools;
+    CheckBox flashlights;
+    CheckBox batteries;
+
+    //Transportation
+    CheckBox bikes;
+    CheckBox busTickets;
+    CheckBox rides;
+
+    //Medical
+    CheckBox pregnancyCare;
+    CheckBox prescriptionMedication;
+    CheckBox nonPrescriptionMedication;
+    CheckBox firstAid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,6 +148,57 @@ public class Confirmation extends AppCompatActivity implements AdapterView.OnIte
         //number = (EditText) findViewById(R.id.number);
         peoplespin = (Spinner) findViewById(R.id.numPeople_spinner);
 
+        //subCategory checkBox initializations
+
+        //Food boxes
+        coffee = (CheckBox) findViewById(R.id.coffeeBox);
+        cannedGoods = (CheckBox) findViewById(R.id.cannedGoodsBox);
+        sandwiches = (CheckBox) findViewById(R.id.sandwichesBox);
+        fruit = (CheckBox) findViewById(R.id.fruitBox);
+        water = (CheckBox) findViewById(R.id.waterBox);
+        granolaBars = (CheckBox) findViewById(R.id.granolaBox);
+
+        //Clothes Boxes
+        jeans = (CheckBox) findViewById(R.id.jeansBox);
+        socks = (CheckBox) findViewById(R.id.socksBox);
+        coats = (CheckBox) findViewById(R.id.coatsBox);
+        hats = (CheckBox) findViewById(R.id.hatsBox);
+        gloves = (CheckBox) findViewById(R.id.glovesBox);
+        blankets = (CheckBox) findViewById(R.id.blanketsBox);
+        shoes = (CheckBox) findViewById(R.id.shoesBox);
+        boots = (CheckBox) findViewById(R.id.bootsBox);
+        underwear = (CheckBox) findViewById(R.id.underwearBox);
+
+        //Toiletries
+        wetWipes = (CheckBox) findViewById(R.id.wetWipesBox);
+        feminineCareProducts = (CheckBox) findViewById(R.id.feminineBox);
+        handSanitizer = (CheckBox) findViewById(R.id.handSanitizerBox);
+        soap = (CheckBox) findViewById(R.id.soapBox);
+        deodorant = (CheckBox) findViewById(R.id.deodorantBox);
+        shampoo = (CheckBox) findViewById(R.id.shampooBox);
+        lotion = (CheckBox) findViewById(R.id.lotionBox);
+        toiletPaper = (CheckBox) findViewById(R.id.toiletPaperBox);
+
+        //Camp Supplies
+        trashBags = (CheckBox) findViewById(R.id.trashBox);
+        tents = (CheckBox) findViewById(R.id.tentBox);
+        sleepingBags = (CheckBox) findViewById(R.id.sleepingBagBox);
+        rope = (CheckBox) findViewById(R.id.ropeBox);
+        tarps = (CheckBox) findViewById(R.id.tarpsBox);
+        tools = (CheckBox) findViewById(R.id.toolsBox);
+        flashlights = (CheckBox) findViewById(R.id.flashLightBox);
+        batteries = (CheckBox) findViewById(R.id.batteriesBox);
+
+        //Transportation
+        bikes = (CheckBox) findViewById(R.id.bikesBox);
+        busTickets = (CheckBox) findViewById(R.id.busTicketsBox);
+        rides = (CheckBox) findViewById(R.id.ridesBox);
+
+        //Medical
+        pregnancyCare = (CheckBox) findViewById(R.id.pregnancyBox);
+        prescriptionMedication = (CheckBox) findViewById(R.id.prescriptionBox);
+        nonPrescriptionMedication = (CheckBox) findViewById(R.id.nonPrescriptionMedicationBox);
+        firstAid = (CheckBox) findViewById(R.id.firstAidBox);
 
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hour_array, android.R.layout.simple_spinner_dropdown_item);
@@ -200,40 +302,126 @@ public class Confirmation extends AppCompatActivity implements AdapterView.OnIte
         //Check which checkbox was clicked
         switch(view.getId()){
             case R.id.foodBox:
-                if (checked)
+                if (checked) {
                     food = true;
-                else
+                    coffee.setVisibility(View.VISIBLE);
+                    cannedGoods.setVisibility(View.VISIBLE);
+                    sandwiches.setVisibility(View.VISIBLE);
+                    fruit.setVisibility(View.VISIBLE);
+                    water.setVisibility(View.VISIBLE);
+                    granolaBars.setVisibility(View.VISIBLE);
+
+                }
+                else {
                     food = false;
+                    coffee.setVisibility(View.GONE);
+                    cannedGoods.setVisibility(View.GONE);
+                    sandwiches.setVisibility(View.GONE);
+                    fruit.setVisibility(View.GONE);
+                    water.setVisibility(View.GONE);
+                    granolaBars.setVisibility(View.GONE);
+                }
                 break;
             case R.id.campBox:
-                if(checked)
+                if(checked) {
                     campingSupplies = true;
-                else
+                    trashBags.setVisibility(View.VISIBLE);
+                    tents.setVisibility(View.VISIBLE);
+                    sleepingBags.setVisibility(View.VISIBLE);
+                    rope.setVisibility(View.VISIBLE);
+                    tarps.setVisibility(View.VISIBLE);
+                    tools.setVisibility(View.VISIBLE);
+                    flashlights.setVisibility(View.VISIBLE);
+                    batteries.setVisibility(View.VISIBLE);
+
+                }else {
                     campingSupplies = false;
+                    trashBags.setVisibility(View.GONE);
+                    tents.setVisibility(View.GONE);
+                    sleepingBags.setVisibility(View.GONE);
+                    rope.setVisibility(View.GONE);
+                    tarps.setVisibility(View.GONE);
+                    tools.setVisibility(View.GONE);
+                    flashlights.setVisibility(View.GONE);
+                    batteries.setVisibility(View.GONE);
+
+                }
                 break;
             case R.id.transportationBox:
-                if(checked)
+                if(checked) {
                     transportation = true;
-                else
+                    bikes.setVisibility(View.VISIBLE);
+                    busTickets.setVisibility(View.VISIBLE);
+                    rides.setVisibility(View.VISIBLE);
+                }else {
                     transportation = false;
+                    bikes.setVisibility(View.GONE);
+                    busTickets.setVisibility(View.GONE);
+                    rides.setVisibility(View.GONE);
+                }
                 break;
             case R.id.medicalBox:
-                if(checked)
+                if(checked) {
                     medical = true;
-                else
+                    pregnancyCare.setVisibility(View.VISIBLE);
+                    prescriptionMedication.setVisibility(View.VISIBLE);
+                    nonPrescriptionMedication.setVisibility(View.VISIBLE);
+                    firstAid.setVisibility(View.VISIBLE);
+                }else {
                     medical = false;
+                    pregnancyCare.setVisibility(View.GONE);
+                    prescriptionMedication.setVisibility(View.GONE);
+                    nonPrescriptionMedication.setVisibility(View.GONE);
+                    firstAid.setVisibility(View.GONE);
+                }
                 break;
             case R.id.clothesBox:
-                if(checked)
+                if(checked) {
                     clothes = true;
-                else
+                    jeans.setVisibility(View.VISIBLE);
+                    socks.setVisibility(View.VISIBLE);
+                    coats.setVisibility(View.VISIBLE);
+                    hats.setVisibility(View.VISIBLE);
+                    gloves.setVisibility(View.VISIBLE);
+                    blankets.setVisibility(View.VISIBLE);
+                    shoes.setVisibility(View.VISIBLE);
+                    boots.setVisibility(View.VISIBLE);
+                    underwear.setVisibility(View.VISIBLE);
+                }else {
                     clothes = false;
+                    jeans.setVisibility(View.GONE);
+                    socks.setVisibility(View.GONE);
+                    coats.setVisibility(View.GONE);
+                    hats.setVisibility(View.GONE);
+                    gloves.setVisibility(View.GONE);
+                    blankets.setVisibility(View.GONE);
+                    shoes.setVisibility(View.GONE);
+                    boots.setVisibility(View.GONE);
+                    underwear.setVisibility(View.GONE);
+                }
                 break;
             case R.id.toiletBox:
-                if(checked)
+                if(checked) {
                     toilet = true;
-                else
+                    wetWipes.setVisibility(View.VISIBLE);
+                    feminineCareProducts.setVisibility(View.VISIBLE);
+                    handSanitizer.setVisibility(View.VISIBLE);
+                    soap.setVisibility(View.VISIBLE);
+                    deodorant.setVisibility(View.VISIBLE);
+                    shampoo.setVisibility(View.VISIBLE);
+                    lotion.setVisibility(View.VISIBLE);
+                    toiletPaper.setVisibility(View.VISIBLE);
+                }else {
                     toilet = false;
+                    wetWipes.setVisibility(View.GONE);
+                    feminineCareProducts.setVisibility(View.GONE);
+                    handSanitizer.setVisibility(View.GONE);
+                    soap.setVisibility(View.GONE);
+                    deodorant.setVisibility(View.GONE);
+                    shampoo.setVisibility(View.GONE);
+                    lotion.setVisibility(View.GONE);
+                    toiletPaper.setVisibility(View.GONE);
+                }
                 break;
         }
     }
